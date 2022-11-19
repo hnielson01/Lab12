c47b04441dcf4068735d862e0b4a57f70f87e4ba
hnielson01 <118557122+hnielson01@users.noreply.github.com>
2022-11-18 21:54:16 -0600

    changes

---
+++ account.py
@@ -0,0 +1,21 @@
+class Account:
+    def __init__(self,name):
+        self.__account_name = 0
+        self.__account_balance = 0
+
+
+    def deposit(self,name):
+        self.account_amount = amount
+        if self.account_amount == 0:
+            self.account_balance = False
+        else:
+            self.account_balance += self.account_amount
+
+    def withdraw(self,amount):
+        self.account_amount = amount
+        if self.account_amount == 0:
+            self.account_balance = False
+        else:
+            self.account_balance -= self.account_amount
+
+
