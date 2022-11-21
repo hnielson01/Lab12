93ee02bb1058285fac7f587f8dad193aabb6d193
hnielson01 <118557122+hnielson01@users.noreply.github.com>
2022-11-20 18:27:22 -0600

    wrote code for test

--- account.py
+++ account.py
@@ -1,21 +1,32 @@
-class Account:
+class account:
+    """
+    A class representing details for an account object
+    """
     def __init__(self,name):
-        self.__account_name = 0
+        self.__account_name = name
         self.__account_balance = 0
 
-
-    def deposit(self,name):
-        self.account_amount = amount
-        if self.account_amount == 0:
-            self.account_balance = False
+    def deposit(self,amount):
+        if amount > 0:
+            self.__account_balance = self.__account_balance + amount
+            return True
         else:
-            self.account_balance += self.account_amount
+            return False
+
 
     def withdraw(self,amount):
-        self.account_amount = amount
-        if self.account_amount == 0:
-            self.account_balance = False
+        if amount > 0:
+            if amount <= self.__account_balance:
+                self.__account_balance = self.account_balance - amount
+                return True
+            else:
+                return False
         else:
-            self.account_balance -= self.account_amount
+            return False
+    def getBalance(self):
+        return self.__account_balance
+
+    def getname(self):
+        return self.__account_name
 
 
